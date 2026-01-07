04d114ff50cf05e180a49703e4277c1df333c817
Charlie Henry <thomashenry@utexas.edu>
2026-01-07 12:43:07 -0600

    add logging for invalid dates

--- data_sources/amanda_closure_publishing.py
+++ data_sources/amanda_closure_publishing.py
@@ -169,8 +169,6 @@ def main(local_file=None):
         )
         .dt.tz_localize(central_time_zone)
     )
-    # Ignores rows with invalid dates
-    closures = closures.dropna(subset=["start_date_dt"])
 
     closures["end_date_dt"] = (
         pd.to_datetime(
@@ -179,7 +177,23 @@ def main(local_file=None):
         )
         .dt.tz_localize(central_time_zone)
     )
+
+    # Logging of invalid dates
+    if closures["start_date_dt"].isna().any() or closures["end_date_dt"].isna().any():
+        invalid = []
+        invalid.append(closures.loc[closures["start_date_dt"].isna(), ["FOLDERRSN", "START_DATE", "SEGMENT_ID"]])
+        invalid.append(closures.loc[closures["end_date_dt"].isna(), ["FOLDERRSN", "END_DATE", "SEGMENT_ID"]])
+        invalid = pd.concat(invalid)
+        for row in invalid.itertuples(index=False):
+            if not pd.isna(row.START_DATE):
+                logger.info(f"Invalid start date ({row.START_DATE}) for folderRSN: {row.FOLDERRSN} and segment "
+                            f"ID: {row.SEGMENT_ID}")
+            if not pd.isna(row.END_DATE):
+                logger.info(f"Invalid end date ({row.END_DATE}) for folderRSN: {row.FOLDERRSN} and segment "
+                            f"ID: {row.SEGMENT_ID}")
+
     # Ignores rows with invalid dates
+    closures = closures.dropna(subset=["start_date_dt"])
     closures = closures.dropna(subset=["end_date_dt"])
 
     work_zones = []
